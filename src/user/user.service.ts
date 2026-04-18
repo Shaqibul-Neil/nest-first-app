f9b1cce7768c99f74c70028261ad56ec2c460153
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-04-18 14:26:18 +0600

    controllers and services

---
+++ src/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  private users = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Jane' },
+  ];
+  getAllUsers() {
+    return this.users;
+  }
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+}
